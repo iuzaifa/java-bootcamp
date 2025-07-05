e281d6f5452968ec9e3fc21a8d8b7aeaa279d1f4
Abu Huzaifa <abuhuzaifaw7@gmail.com>
2025-07-06 02:52:47 +0530

    distinct update

--- src/Day_08_java_streams/StreamCollectors.java
+++ src/Day_08_java_streams/StreamCollectors.java
@@ -3,5 +3,19 @@ package Day_08_java_streams;
 public class StreamCollectors {
     public static void main(String[] args) {
 
+        /*
+
+            Collectors is a utility class that provides common implementations of
+            the Collector interface — used with stream.collect().
+
+            Syntax :
+            <T, A, R> R collect(Collector<? super T, A, R> collector)
+            List<String> names = stream.collect(Collectors.toList());
+
+
+
+
+         */
+
     }
 }
